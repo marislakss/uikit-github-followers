0a78beef470173916785a0b81e7f3586febc64fc
Maris Lakss <maris.lakss@gmail.com>
2023-11-05 14:52:12 +0200

    Add UITextField i.e. GFTextField

---
+++ GitHubFollowers/GFTextField.swift
@@ -0,0 +1,45 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by Māris Lakšs on 05/11/2023.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configure()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        layer.cornerRadius        = 10
+        layer.borderWidth         = 2
+        layer.borderColor         = UIColor.systemGray4.cgColor
+
+        textColor                 = .label
+        tintColor                 = .label
+        textAlignment             = .center
+        // Use Dynamic Type
+        font                      = UIFont.preferredFont(forTextStyle: .title2)
+
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize           = 12
+
+        backgroundColor           = .tertiarySystemBackground
+
+        // Having different types of usernames turn off autocorrect for this text field
+        autocorrectionType        = .no
+
+        placeholder               = "Enter a username"
+    }
+}
