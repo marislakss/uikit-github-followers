ca9de4ae3bf297de6b4ca4d9264bec5c62f97ed6
Maris Lakss <maris.lakss@gmail.com>
2024-04-14 20:24:00 +0300

    Refactor Date Formatter in Date+Ext using new iOS 15 syntax

--- GitHubFollowers/Extensions/Date+Ext.swift
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -9,13 +9,16 @@ import Foundation
 
 extension Date {
     
-    // This function will convert the date to a string,
-    func convertToMonthYearFormat() -> String {
-        // Create a date formatter
-        let dateFormatter = DateFormatter()
-        // Set the date format
-        dateFormatter.dateFormat = "MMM yyyy"
-        // Return the date
-        return dateFormatter.string(from: self)
-    }
+//    // This function will convert the date to a string,
+//    func convertToMonthYearFormat() -> String {
+//        // Create a date formatter
+//        let dateFormatter = DateFormatter()
+//        // Set the date format
+//        dateFormatter.dateFormat = "MMM yyyy"
+//        // Return the date
+//        return dateFormatter.string(from: self)
+//    }
+
+    // New iOS 15 way to format date
+    func convertToMonthYearFormat() -> String { formatted(.dateTime.month().year()) }
 }
